924919a2fb3e9466d63926f17276cf242a73b301
JoelChango <joelchangobarzola@gmail.com>
2025-12-05 13:48:59 +0100

    Add files via upload

---
+++ prueba1.js
@@ -0,0 +1,15 @@
+   // soy un comentario en linea
+             /* -------------------CSS*/
+            /*    j-------*/;
+            var mensaje = "hola mundo que tal";
+            console.log(mensaje);
+            console.log(mensaje);
+            console.log(document);
+            console.log(localStorage)
+
+            var esViernes = true;
+            console.log("Es viernes y quiero beber"+ esViernes);
+            //debugger
+            if(esViernes == true){
+                console.log("Pues si es viernes y tengo sed");
+            } 
